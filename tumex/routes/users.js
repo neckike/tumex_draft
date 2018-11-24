97dfd1dab39c068bff7a8ce7e09855a3b700b1dc
hectorgerardo.munozhernandez <nec_ge99@hotmail.com>
2018-11-24 23:38:21 +0100

    ya encuentra usuariosgit add --allgit add --all

--- tumex/routes/users.js
+++ tumex/routes/users.js
@@ -5,6 +5,7 @@ var redis = require('redis');
 var User = require('../models/user');
 let client = redis.createClient();
 //let client = require('./index');
+var tmp_user;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -21,7 +22,7 @@ router.get('/streaming', ensureAuthenticated, function(req, res, next) {
 });
 
 router.get('/friends', ensureAuthenticated, function(req, res, next) {
-  res.render('friends', {title:'Friends'});
+  res.render('friends', {title:'Friends', message: req.flash('message')});
 });
 
 router.get('/torneos', ensureAuthenticated, function(req, res, next) {
@@ -35,9 +36,37 @@ router.get('/logout', function(req, res){
 });
 
 router.get('/editprofile', ensureAuthenticated, function(req, res, next) {
+
   res.render('editprofile', {title:'EditProfile'});
 });
 
+router.get('/searchfriends', ensureAuthenticated, function(req, res, next) {
+  res.locals.searchedUser = tmp_user;
+  res.render('friends', {title:'Friends', message: req.flash('message')});
+});
+
+router.post('/searchfriends', function(req, res, next) {
+
+  var username = req.body.search;
+
+  //process.nextTick(function(){
+
+    User.getUserByUsername(username, function(err, user){
+      if(err) throw err;
+      if(!user){
+        console.log('Usuario no encontrado');
+        tmp_user=null;
+      }
+      else{
+        tmp_user=user;
+        console.log('Nombre: '+user.nombre + ' '+user.apellido);
+      }
+    });
+
+    res.location('/users/searchfriends');
+    res.redirect('/users/searchfriends');
+  //});
+});
 router.post('/editprofile', function(req, res, next) {
 	console.log('si entre');
   var edad = req.body.edad;
@@ -81,5 +110,4 @@ function ensureAuthenticated(req, res, next){
   }
   res.redirect('/');
 }
-
 module.exports = router;
